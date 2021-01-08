247617ba71b14fb04de0e113b113070ab7be95fa
NaglaEssam12 <59181709+NaglaEssam12@users.noreply.github.com>
2021-01-08 15:17:18 +0200

    Update Message.java

--- officeHoursManagement/src/java/model/Message.java
+++ officeHoursManagement/src/java/model/Message.java
@@ -5,10 +5,59 @@
  */
 package model;
 
+
+import java.time.LocalDate;
+
 /**
  *
  * @author User
  */
 public class Message {
+    int messageID;
+    int fromUserID;
+    int toUserID;
+    String actualMassage;
+    LocalDate date;
+
+    public int getMessageID() {
+        return messageID;
+    }
+
+    public void setMessageID(int messageID) {
+        this.messageID = messageID;
+    }
+
+    public int getFromUserID() {
+        return fromUserID;
+    }
+
+    public void setFromUserID(int fromUserID) {
+        this.fromUserID = fromUserID;
+    }
+
+    public int getToUserID() {
+        return toUserID;
+    }
+
+    public void setToUserID(int toUserID) {
+        this.toUserID = toUserID;
+    }
+
+    public String getActualMassage() {
+        return actualMassage;
+    }
+
+    public void setActualMassage(String actualMassage) {
+        this.actualMassage = actualMassage;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
     
 }
