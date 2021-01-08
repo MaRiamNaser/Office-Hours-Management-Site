893bb78578cf05cc4b83003dccf647a29b6555b6
NaglaEssam12 <59181709+NaglaEssam12@users.noreply.github.com>
2021-01-08 15:17:49 +0200

    Update OfficeHours.java

--- officeHoursManagement/src/java/model/OfficeHours.java
+++ officeHoursManagement/src/java/model/OfficeHours.java
@@ -5,10 +5,76 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author User
  */
 public class OfficeHours {
+    int officeHoursID;
+    String fromTime;
+    String toTime;
+    LocalDate date;
+    String state;
+    String location;
+    int userID;
+
+    public int getOfficeHoursID() {
+        return officeHoursID;
+    }
+
+    public void setOfficeHoursID(int officeHoursID) {
+        this.officeHoursID = officeHoursID;
+    }
+
+    public String getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(String fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public String getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(String toTime) {
+        this.toTime = toTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
     
 }
