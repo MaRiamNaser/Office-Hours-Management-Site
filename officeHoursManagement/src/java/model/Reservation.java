be2c8e271bdecdfe0d366a017b8741d1e3e42a4a
NaglaEssam12 <59181709+NaglaEssam12@users.noreply.github.com>
2021-01-08 15:18:19 +0200

    Update Reservation.java

--- officeHoursManagement/src/java/model/Reservation.java
+++ officeHoursManagement/src/java/model/Reservation.java
@@ -10,5 +10,42 @@ package model;
  * @author User
  */
 public class Reservation {
+    int reservationID;
+    int officeHoursID;
+    int fromUserID;
+    int toUserID;
+
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public int getOfficeHoursID() {
+        return officeHoursID;
+    }
+
+    public void setOfficeHoursID(int officeHoursID) {
+        this.officeHoursID = officeHoursID;
+    }
+
+    public int getFromUserID() {
+        return fromUserID;
+    }
+
+    public void setFromUserID(int fromUserID) {
+        this.fromUserID = fromUserID;
+    }
+
+    public int getToUserID() {
+        return toUserID;
+    }
+
+    public void setToUserID(int toUserID) {
+        this.toUserID = toUserID;
+    }
+    
     
 }
